8f8eda49c11e626eed20d2378a3940dd53f1b6c2
Hrithick-Kumar <kumarhrithick369@gmail.com>
2026-06-11 08:51:44 +0530

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import requests
 st.header("ChatBot")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
 def Chat_Ai(User_Chat):
   api_url="https://openrouter.ai/api/v1/chat/completions"
   api_key=st.secrets["ChatBot"]
@@ -61,13 +63,13 @@ Your primary goal is to make the user feel understood, supported, comfortable, a
   if "choices" in result:
     return result["choices"][0]["message"]["content"]
 User_Chat=st.chat_input("Chat with your friend")
-if User_Chat is not None:
+if User_Chat:
   with st.chat_message("user"):
     st.markdown(User_Chat)
-    st.session_state["UserChat"]=User_Chat
+    st.session_state["messages"]=st.session_state["messages"].append({"role":"user","content":User_Chat})
   with st.spinner("Replying"):
     answer=Chat_Ai(User_Chat)
     with st.chat_message("Assistant"):
       st.markdown(answer)
-      st.session_state["FriendReply"]=answer
-
+      st.session_state["messages"]=st.session_state["messages"].append({"role":"system","content":answer})
+ 
