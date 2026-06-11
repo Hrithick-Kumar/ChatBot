6f9063466ae102dee31432cc665af82af28368f8
Hrithick-Kumar <kumarhrithick369@gmail.com>
2026-06-11 08:17:26 +0530

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -65,7 +65,7 @@ if User_Chat is not None:
   with st.chat_message("user"):
     st.markdown(User_Chat)
     st.session_state["UserChat"]=User_Chat
-    st.markdown(st.session_state["UserChat"]
+    st.markdown(st.session_state["UserChat"])
   with st.spinner("Replying"):
     answer=Chat_Ai(User_Chat)
     with st.chat_message("Assistant"):
